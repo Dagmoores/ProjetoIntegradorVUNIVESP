fee82189b0db60bae7f5a3a1e8b03cfc81bbdbb6
Daniel Augusto Moore da Silva <dagmoores@gmail.com>
2023-10-18 23:18:09 -0300

    sensor de temperatura adicionado ao projeto. O próximo passo envolverá modificar o código para ler a temperatura apenas em caso de movimento

--- ProjetoIntegrador/ProjetoIntegrador.ino
+++ ProjetoIntegrador/ProjetoIntegrador.ino
@@ -1,3 +1,13 @@
+#include <DHT.h>
+
+//definir o tipo de sensor como DHT11
+#define DHT_TYPE DHT11
+
+//definir o pino conectado ao DHT11
+#define DHT_PIN 27
+
+DHT dht(DHT_PIN, DHT_TYPE);
+
 int movimentSensor = 12;
 int ledMovimentDetected = 13;
 int ledSystem = 14;
@@ -11,31 +21,48 @@ void setup() {
   pinMode(movimentSensor, INPUT);
 
   Serial.begin(9600);
+  dht.begin();
 }
 
 void loop() {
+
+
+  // INICIO SENSOR DE TEMPERATURA
+  float temperature = dht.readTemperature();
+
+  if(isnan(temperature)) { 
+    Serial.println("Erro na leitura do sensor de temperatura e umidade");
+  } else {
+    Serial.print("Temperatura: "); 
+    Serial.print(temperature); 
+    Serial.println(" °C");
+  };
+  // FIM SENSOR DE TEMPERATURA
+
+
   sensorState = digitalRead(movimentSensor);
   Serial.println(sensorState);
 
  
   if(sensorState == HIGH) {
     digitalWrite(ledMovimentDetected, HIGH); // acende a luz que demonstra que o sensor foi ativado
-    delay(1000);
 
     if (systemState == LOW) {
       Serial.println("Sistema ativado!");  
       digitalWrite(ledSystem, HIGH); // ativa o led que demonstra que se sistema de controle térmico está operante
-      delay(1500);
+      // delay(1500);
       systemState = HIGH;                     // registra o sistema como ativo
     } else { // se o sensor for ativado novamente enquanto o sistema já está em funcionamento, significa que o animal saiu do espaço, portanto o sistema deve ser desligado!
       Serial.println("Sistema desativado!"); 
       digitalWrite(ledSystem, LOW); // desativa o led que demonstra que se sistema de controle térmico está operante
-      delay(1500);
+      // delay(1500);
       systemState = LOW; // registra o sistema como desativado
     }
   }
     else {
         digitalWrite(ledMovimentDetected, LOW); // apaga a luz que demonstra que o sensor foi ativado
     }
-  }
+
+    delay(2000); // 2 seg de delay entre as leituras
+}
   
