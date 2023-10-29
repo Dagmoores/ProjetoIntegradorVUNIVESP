a8f45407436eaa91686df04685ac06d5f849477a
Daniel Augusto Moore da Silva <dagmoores@gmail.com>
2023-10-29 16:42:53 -0300

    implementação do critério de temperatura para inicio do sistema, de uma função de segurança para evitar que o sistema permaneça ligado em baixas temperaturas e mudanças no código para melhorar a legibilidade e usabilidade do mesmo com o estabelecimento de funções

--- ProjetoIntegrador/ProjetoIntegrador.ino
+++ ProjetoIntegrador/ProjetoIntegrador.ino
@@ -1,27 +1,51 @@
 #include <DHT.h>
 
-//definir o tipo de sensor como DHT11
-#define DHT_TYPE DHT11
-
 //definir o pino conectado ao DHT11
 #define DHT_PIN 27
 
-// definir o pino do cooler
+//definir o tipo de sensor como DHT11
+#define DHT_TYPE DHT11
+
 // este cooler tem lógica invertida, então fica ativo no estado "LOW"
 #define coolerPIN 26
 
+//sensor de movimento
+#define movimentSensor 12
+
+//led de detecção de movimento
+#define ledMovimentDetected 13
+
+//led de sistema ativo
+#define ledSystem 14
+
 DHT dht(DHT_PIN, DHT_TYPE);
 
-int movimentSensor = 12;
-int ledMovimentDetected = 13;
-int ledSystem = 14;
+//estados iniciais
 int systemState = LOW;
 int sensorState = 0;
 
- 
 
+//função que ligará o sistema
+  void ligar() {
+      Serial.println("Sistema ativado!");  
+      digitalWrite(coolerPIN, LOW);
+      digitalWrite(ledSystem, HIGH); 
+      // delay(1000);
+      systemState = HIGH; // registra o sistema como ativo
+  }
+
+
+//função que desligará o sistema
+  void desligar() {
+      Serial.println("Sistema desativado!"); 
+      digitalWrite(coolerPIN, HIGH);
+      digitalWrite(ledSystem, LOW);
+      // delay(1000);
+      systemState = LOW; // registra o sistema como desativado
+  }
 
 void setup() {
+
   pinMode(ledMovimentDetected, OUTPUT);
   pinMode(ledSystem, OUTPUT);
   pinMode(movimentSensor, INPUT);
@@ -30,8 +54,6 @@ void setup() {
 
   Serial.begin(9600);
   dht.begin();
-
-  
 }
 
 void loop() {
@@ -54,24 +76,23 @@ void loop() {
 
  
   if(sensorState == HIGH) {
-    digitalWrite(ledMovimentDetected, HIGH); // acende a luz que demonstra que o sensor foi ativado
+    digitalWrite(ledMovimentDetected, HIGH); 
 
-    if (systemState == LOW) {
-      Serial.println("Sistema ativado!");  
-      digitalWrite(coolerPIN, LOW);
-      digitalWrite(ledSystem, HIGH); // ativa o led que demonstra que se sistema de controle térmico está operante
-      delay(1500);
-      systemState = HIGH;                     // registra o sistema como ativo
-    } else { // se o sensor for ativado novamente enquanto o sistema já está em funcionamento, significa que o animal saiu do espaço, portanto o sistema deve ser desligado!
-      Serial.println("Sistema desativado!"); 
-      digitalWrite(coolerPIN, HIGH);
-      digitalWrite(ledSystem, LOW); // desativa o led que demonstra que se sistema de controle térmico está operante
-      delay(1500);
-      systemState = LOW; // registra o sistema como desativado
+    if (systemState == LOW && temperature > 30) {
+      ligar();
+    } else { 
+      desligar();
     }
   }
     else {
-        digitalWrite(ledMovimentDetected, LOW); // apaga a luz que demonstra que o sensor foi ativado
+        digitalWrite(ledMovimentDetected, LOW);
     }
+
+  // condição de segurança do sistema para impedir que ele chegue a temperatura muito baixa enquanto ligado.
+  if(temperature < 21) {
+    desligar();
+  }
+
+  delay(1000);
 }
   
